13b757c5f6071f93e201afa9f8e3378b97f60c0f
Jose Gilibert <jotaemegeuve@gmail.com>
2019-02-12 15:09:34 +0100

    ejercicios hechos y fixeao todo

--- src/main/java/org/springframework/samples/petclinic/JDBCApplication.java
+++ src/main/java/org/springframework/samples/petclinic/JDBCApplication.java
@@ -2,6 +2,7 @@ package org.springframework.samples.petclinic;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -12,7 +13,7 @@ public class JDBCApplication {
 		System.out.println("-------- Test de conexión con MySQL ------------");
 
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
+			Class.forName("com.mysql.cj.jdbc.Driver");
 		} catch (ClassNotFoundException e) {
 			System.out.println("No encuentro el driver en el Classpath");
 			e.printStackTrace();
@@ -27,6 +28,60 @@ public class JDBCApplication {
 			if (connection != null)
 				System.out.println("Conexión establecida");
 			
+				statement = connection.createStatement();
+				// listar a tol mundo
+				String sql = "SELECT * FROM owners";
+				ResultSet rs = statement.executeQuery(sql);
+				while(rs.next()) {
+					int id = rs.getInt("id");
+					String firstName = rs.getString("first_name");
+					String lastName = rs.getString("last_name");
+					String address = rs.getString("address");
+					String city = rs.getString("city");
+					String telephone = rs.getString("telephone");
+					
+					System.out.print("Id: "+ id);
+					System.out.print(", Nombre: "+ firstName);
+					System.out.print(", Apellidos: "+ lastName);
+					System.out.print(", Dirección: "+ address);
+					System.out.print(", Ciudad: "+ city);
+					System.out.print(", Teléfono: "+ telephone + "\n\n");
+				}
+				// añadirme
+				sql = "INSERT INTO owners (first_name, last_name, address, city, telephone)" +
+				" VALUES ('Jose', 'Gilibert', 'Ramon y Cajal', 'Sevilla', '617545989')";
+				//statement.executeUpdate(sql);
+				
+				//buscar dueños que coincidan
+				sql = "SELECT * FROM owners WHERE first_name LIKE ? OR last_name LIKE ?";
+				String busqueda = "Da";
+				String termino = "%"+busqueda+"%";
+				PreparedStatement preparedStatement = connection.prepareStatement(sql);
+				preparedStatement.setString(1, termino); //nombre
+				preparedStatement.setString(2, termino); //apellido
+				System.out.print("Coincidencias con "+busqueda+": \n");
+				rs = preparedStatement.executeQuery();
+				
+				while(rs.next()) {
+					int id = rs.getInt("id");
+					String firstName = rs.getString("first_name");
+					String lastName = rs.getString("last_name");
+					
+					System.out.print("Id: "+ id);
+					System.out.print(", Nombre: "+ firstName);
+					System.out.print(", Apellidos: "+ lastName+"\n");
+				}
+				
+				// añadir owner variable
+				/* sql = "INSERT INTO owners (first_name, last_name, address, city, telephone)" +
+				" VALUES (?, ?, ?, ?, ?)";
+				PreparedStatement preparedStatement = connection.prepareStatement(sql);
+				preparedStatement.setString(1, "Paco"); //nombre
+				preparedStatement.setString(2, "Pérez"); //apellido
+				preparedStatement.setString(3, "Calle Ruina"); //direccion
+				preparedStatement.setString(4, "Madrid"); //ciudad
+				preparedStatement.setString(5, "666696969"); //telefono
+				preparedStatement.executeUpdate(); */
 		    
 		} catch (SQLException e) {
 			System.out.println("Connection Failed! Check output console");
@@ -49,3 +104,4 @@ public class JDBCApplication {
 	}
 
 }
+
